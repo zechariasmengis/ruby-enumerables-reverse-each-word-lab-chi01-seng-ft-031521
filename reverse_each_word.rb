ccfcecb010b3d8b2e32033d4e86778c823c3c4d8
Zecharias Mengis <zmengis@gmail.com>
2021-02-17 18:18:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,8 @@
-def reverse_each_word(sentence)
-  reversed_array = []
-  split_array = sentence.split(' ')
-  split_array.each do |word|
-    word.reverse << reversed_array
-  end
-  reversed_array
-end
 
-=begin def reverse_each_word(sentence)
+def reverse_each_word(sentence)
   split_array = sentence.split(' ')
   sentence_array = split_array.collect do |word|
     word.reverse
   end
   sentence_array.join(' ')
 end
-=end
